5cb511ce41bcf404cd0c0c0c2b50c455559b7e29
mohaak47 <147770477+mohaak47@users.noreply.github.com>
2023-10-16 13:37:34 +0330

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,70 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 const app = express()
 
 app.use(cors())
 app.use(express.static('dist'))
 
 
-let notes = [
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  'mongodb+srv://mohaak:' + password + '@cluster0.juukqdd.mongodb.net/noteApp?retryWrites=true&w=majority'
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const noteList = [
   {
-    id: 1,
     content: "HTML is easy",
     important: true
   },
   {
-    id: 2,
     content: "Browser can execute only JavaScript",
     important: false
   },
   {
-    id: 3,
     content: "GET and POST are the most important methods of HTTP protocol",
     important: true
+  },
+  {
+    content: "React ia a frontend framework",
+    important: true
+  },
+  {
+    content: "Express ia a backendend framework",
+    important: false
   }
 ]
+
+const notes = noteList.map(note => {
+  const dbNote = new Note(note)
+  dbNote.save().then(result => {
+  console.log('note saved!')
+  })
+})
+
 
  app.get('/', (request, response) => {
   response.send('<h1> Hello,World of Web Programming </h1>')
 })
   app.get('/api/notes', (request, response) => {
+    Note.find({}).then(notes => {
     response.json(notes)
+  })
 })
 
   app.get('/api/notes/:id', (request, response) => {
