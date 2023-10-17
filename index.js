e32a8461815c32c3e5bbbcd8a32f2a914b08f67d
mohaak47 <147770477+mohaak47@users.noreply.github.com>
2023-10-17 09:57:24 +0330

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
+const Note = require('./models/note')
+
 const app = express()
 
 app.use(cors())
 
-const url = process.env.MONGODB_URI
-console.log('connecting to', url)
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
-        .then(result => {
-          console.log('connected to MongoDB')
-        })
-        .catch((error) => {
-          console.log('error connecting to MongoDB', error.message)
-        })
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON',{
-  transform:(document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
 
 const noteList = [
   {
