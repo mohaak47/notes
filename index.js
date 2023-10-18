356e92a7d55b4aaf64d7bbfeeaa7c7bfb579aa7a
mohaak47 <147770477+mohaak47@users.noreply.github.com>
2023-10-18 08:36:03 +0330

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const Note = require('./models/note')
 
 const app = express()
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(cors())
+app.use(express.json())
 app.use(express.static('dist'))
 
 
@@ -28,26 +33,12 @@ const notes = noteList.map(note => {
 
 
 
-
- app.get('/', (request, response) => {
-  response.send('<h1> Hello,World of Web Programming </h1>')
-})
   app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
-    response.json(notes)
+      response.json(notes)
   })
 })
 
-  app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find( note => note.id === id)
-    if (note) {
-        response.json(note)
-    }
-    else {
-      response.status(404).end()
-    }
-  })
 
   app.post('/api/notes', (request,response) => {
     const body = request.body
@@ -64,7 +55,20 @@ const notes = noteList.map(note => {
       response.json(savedNote)
     })
   } )
+  app.get('/api/notes/:id', (request, response) => {
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
+  })
 
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+  })
+
+  app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT,() => {
